f656bd9fbd2b8cc505d6fca97c2b0d41d322ba05
simplegr33n <gcgolda@gmail.com>
2017-01-24 13:07:21 -0800

    added searchbar

--- app/src/main/java/ca/ggolda/reference_criminal_code/ActivityMain.java
+++ app/src/main/java/ca/ggolda/reference_criminal_code/ActivityMain.java
@@ -1,12 +1,18 @@
 package ca.ggolda.reference_criminal_code;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import java.util.List;
 
@@ -22,8 +28,13 @@ public class ActivityMain extends AppCompatActivity {
     private AdapterHeading mAdapterHeading;
     private ListView mListViewHeadings;
 
+    private ImageView mBtnSearch;
+    private ImageView mBtnSearchOpen;
+
     private ImageView mBtnParts;
     public static LinearLayout mParts;
+
+    private RelativeLayout layoutSearchbar;
 
     DbHelper dbHelper;
 
@@ -47,6 +58,12 @@ public class ActivityMain extends AppCompatActivity {
         mListViewHeadings = (ListView) findViewById(R.id.listview_heading);
         mListViewHeadings.setAdapter(mAdapterHeading);
 
+        layoutSearchbar = (RelativeLayout) findViewById(R.id.lyt_search);
+        mBtnSearchOpen = (ImageView) findViewById(R.id.btn_search_open);
+        mBtnSearch = (ImageView) findViewById(R.id.btn_search);
+
+
+
         // bring parts up or down
         mBtnParts.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +71,25 @@ public class ActivityMain extends AppCompatActivity {
                 partsHideShow();
             }
         });
+
+        // bring parts up or down
+        mBtnSearchOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layoutSearchbar.setVisibility(View.VISIBLE);
+            }
+        });
+
+        // bring parts up or down
+        mBtnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layoutSearchbar.setVisibility(View.GONE);
+            }
+        });
+
+
+
 
     }
 
@@ -65,6 +101,24 @@ public class ActivityMain extends AppCompatActivity {
             mParts.setVisibility(View.GONE);
             partsVisible = 0;
         }
+    }
+
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if ( v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent( event );
     }
 
     @Override
